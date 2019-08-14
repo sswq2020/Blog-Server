c49cc82aea062ce6dc9519fd095e80a841d0b278
sswq2020 <a309108243@qq.com>
2019-08-14 09:07:11 +0800

    去掉多余的代码

--- src/router/user.js
+++ src/router/user.js
@@ -2,14 +2,6 @@ const {login} = require("../controller/user")
 const { SuccessModel, ErrorModel } = require("../model/resModel")
 const {set} = require('../db/redis')
 
-// 获取 cookie 的过期时间
-const getCookieExpires = () => {
-    const d = new Date()
-    d.setTime(d.getTime() + (24 * 60 * 60 *1000))
-    return d.toGMTString()
-}
-
-
 const handleUserRouter = (req, res) => {
     const method = req.method
     const path = req.path
