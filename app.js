8e95f69710ec25b448d4c1e99e1418d0f0923c79
shiswq@hletong.com <shiswq@hletong.com>
2019-10-18 09:38:50 +0800

    feat: 🎸 通过配置项将seesion存储在redis里

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 /*****日志功能****/
 const logger = require('morgan');
+/*****SESSION****/
+const session = require("express-session")
+/****创建redis的客户端***/
+const redisClient =  require("./db/redis")
+/****session连接redis***/
+const RedisStore = require('connect-redis')(session) // 用法参照https://www.npmjs.com/package/connect-redis
 
 const indexRouter = require('./routes/index');
 const blogRouter = require('./routes/blog');
@@ -17,6 +23,22 @@ app.use(logger('dev'));
 app.use(express.json()); // 通过express.json(),可以将post请求传递的参数,放入req.body中,比起原生node的getPostData函数,一行代码解决
 app.use(express.urlencoded({ extended: false }));// 因为上面是接收req.headers['content-type'] === 'application/json',而没有post还有x-www-form等其他形式的,这里做这个处理
 app.use(cookieParser());
+
+/****redis的存储器***/
+const sessionStore = new RedisStore({
+  client:redisClient
+})
+
+app.use(session({
+  secret:'SSwq_2332#',
+  cookie:{
+    path:'/', // 默认配置
+    httpOnly:true, // 默认配置
+    maxAge:10 * 60 * 1000
+  },
+  store:sessionStore //之前没有redis，session存在内存中,现在可以放在redis数据库里
+
+}))
 
 app.use('/', indexRouter);
 app.use('/api/blog', blogRouter);
