e422392f5524dedbe95db9b21d0332654741d59f
sswq2020 <a309108243@qq.com>
2020-05-11 07:52:52 +0800

    初始化koa的配置

--- app.js
+++ app.js
@@ -1,81 +1,44 @@
-/****专门用来处理404***/
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-/*****解析cookie****/
-const cookieParser = require('cookie-parser');
-/*****日志功能****/
-const logger = require('morgan');
-/*****SESSION****/
-const session = require("express-session")
-/****创建redis的客户端***/
-const redisClient =  require("./db/redis")
-/****session连接redis***/
-const RedisStore = require('connect-redis')(session) // 用法参照https://www.npmjs.com/package/connect-redis
-/****获取环境变量***/
-const ENV = process.env.NODE_ENV
-const blogRouter = require('./routes/blog');
-const userRouter = require('./routes/user');
-/*****app可以这么理解,每次服务端监听后生成的实例****/
-let app = express();
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
-
-if(ENV !== 'production'){
-  // 开发环境,测试环境
-  app.use(logger('dev'));
-}else{
-  // 线上环境
-  const logFileName = path.join(__dirname,'logs','access.log')
-  const writeStream = fs.createWriteStream(logFileName,{
-    flags:'a'
-  })
-  app.use(logger('combined',{
-    stream: writeStream
-  }))
-}
-
-
-app.use(express.json()); // 通过express.json(),可以将post请求传递的参数,放入req.body中,比起原生node的getPostData函数,一行代码解决
-app.use(express.urlencoded({ extended: false }));// 因为上面是接收req.headers['content-type'] === 'application/json',而没有post还有x-www-form等其他形式的,这里做这个处理
-app.use(cookieParser());
-
-/****redis的存储器***/
-const sessionStore = new RedisStore({
-  client:redisClient
-})
-
-app.use(session({
-  secret:'SSwq_2332#',
-  cookie:{
-    path:'/', // 默认配置
-    httpOnly:true, // 默认配置
-    maxAge:60 * 60 * 1000
-  },
-  store:sessionStore //之前没有redis，session存在内存中,现在可以放在redis数据库里
-
-}))
-
-app.use('/api/blog', blogRouter);
-app.use('/api/user', userRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const index = require('./routes/index')
+const users = require('./routes/users')
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+onerror(app)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
 });
 
-module.exports = app;
+module.exports = app
--- middleware/loginCheck.js
+++
@@ -1,9 +0,0 @@
-const {createErrorData} = require('../model/resModel')
-
-module.exports = (req,res,next) =>{
-    if(req.session.username) {
-        next()
-        return 
-    }
-    res.json(createErrorData("未登录"))
-}
